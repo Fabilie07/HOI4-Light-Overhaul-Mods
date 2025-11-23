106211579da2d0a9815eb507fb20ad1f5490512b
Fabilie07 <fabilie.gaming@gmail.com>
2025-11-23 23:32:32 +0100

    Update 23.11. v2
    Module Handling is working up until now. All add_stats arguments are read correctly.

--- light-overhaul-customizer/_scripts/module_list.gd
+++ light-overhaul-customizer/_scripts/module_list.gd
@@ -56,6 +56,27 @@ func get_module_strings() -> PackedStringArray:
 	
 	return module_string_array
 
+# Return an Array of all Lines in order without all comments, tabs and emptyspaces
+func uncomment_lines(commented_string: String) -> PackedStringArray:
+	var line_array = commented_string.split("\n")
+	var line_uncommented_array: PackedStringArray
+	
+	for line in line_array:
+		var line_uncommented = ""
+		for character in line:
+			if character == "#":
+				break
+			else:
+				line_uncommented = line_uncommented + character
+		
+		for del in ["\t", "\r", " "]:
+				line_uncommented = line_uncommented.replace(del, "")
+		
+		if line_uncommented != "":
+			line_uncommented_array.append(line_uncommented)
+	
+	return line_uncommented_array
+
 func rec_load_modules(remaining_code: PackedStringArray, bracket_level: int):
 	if remaining_code.size() > 0:
 		if bracket_level == 0:
@@ -91,27 +112,66 @@ func rec_load_modules(remaining_code: PackedStringArray, bracket_level: int):
 						bracket_level -= 1
 					module_strings.append(remaining_code[0])
 					remaining_code.remove_at(0)
-				print(module_strings)
+				read_module(module_name, module_strings)
 				
 				rec_load_modules(remaining_code, 1)
 
-# Return an Array of all Lines in order without all comments, tabs and emptyspaces
-func uncomment_lines(commented_string: String) -> PackedStringArray:
-	var line_array = commented_string.split("\n")
-	var line_uncommented_array: PackedStringArray
+func read_module(module_name: String, module_strings: PackedStringArray):
+	var equipment = 0
+	var category = ""
+	var gui_category = ""
+	var equipment_type = ""
+	var gfx = ""
+	var sfx = ""
+	var stats = {}
+	var stats_mult = {}
+	var mission_stats = {}
+	var mission_stats_mult = {}
+	var mission_allowed = []
+	var category_forbidden = []
+	var category_allowed = {}
+	var cost = 0
 	
-	for line in line_array:
-		var line_uncommented = ""
-		for character in line:
-			if character == "#":
-				break
+	# 0: Module 1: add_stats 2: multiply_stats 3: mission_type_stats 4: mission_type_stats/limit
+	# 5: mission_type_stats/add_stats 6: mission_type_stats/multiply_stats
+	var bracket_open = 0
+	
+	for line in module_strings:
+		if bracket_open == 0:
+			if line.contains("category=") and not line.contains("gui_category="):
+				category = line.replace("category=", "")
+			elif line.contains("gui_category="):
+				gui_category = line.replace("gui_category=", "")
+			elif line.contains("add_equipment_type="):
+				equipment_type = line.replace("add_equipment_type=", "")
+			elif line.contains("gfx="):
+				gfx = line.replace("gfx=", "")
+			elif line.contains("sfx="):
+				sfx = line.replace("sfx=", "")
+			elif line.contains("xp_cost="):
+				cost = line.replace("xp_cost=", "")
+				
+			elif line.contains("add_stats={"):
+				bracket_open = 1
+				if line != "add_stats={":
+					var temp_line = line.replace("add_stats={", "")
+					if temp_line.contains("}"):
+						bracket_open = 0
+						temp_line = temp_line.replace("}", "")
+					stats.merge(stat_handler(temp_line))
+			
+		elif bracket_open == 1:
+			if line.contains("}"):
+				bracket_open = 0
+				if line != "}":
+					var temp_line = line.replace("}", "")
+					stats.merge(stat_handler(temp_line))
 			else:
-				line_uncommented = line_uncommented + character
-		
-		for del in ["\t", "\r", " "]:
-				line_uncommented = line_uncommented.replace(del, "")
-		
-		if line_uncommented != "":
-			line_uncommented_array.append(line_uncommented)
+				stats.merge(stat_handler(line))
 	
-	return line_uncommented_array
+	print(stats)
+
+func stat_handler(stat_string: String) -> Dictionary:
+	var stat_array = stat_string.split("=")
+	
+	return {stat_array[0]: stat_array[1].to_float()}
